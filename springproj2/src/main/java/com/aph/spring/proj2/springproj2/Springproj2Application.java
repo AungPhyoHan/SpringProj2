57d7aca86d4160885b435b0f266587c23355c116
AungPhyoHan <hehehaha22899@gmail.com>
2025-01-19 22:52:45 +0630

    start

---
+++ springproj2/src/main/java/com/aph/spring/proj2/springproj2/Springproj2Application.java
@@ -0,0 +1,13 @@
+package com.aph.spring.proj2.springproj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springproj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springproj2Application.class, args);
+	}
+
+}
---
+++ springproj2/src/test/java/com/aph/spring/proj2/springproj2/Springproj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aph.spring.proj2.springproj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
