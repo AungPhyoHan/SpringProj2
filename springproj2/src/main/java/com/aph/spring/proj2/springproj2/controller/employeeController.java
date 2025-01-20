5c923b1a75d4579522668cb2605aeada98ed61ac
AungPhyoHan <hehehaha22899@gmail.com>
2025-01-20 19:31:05 +0630

    update

--- springproj2/src/main/java/com/aph/spring/proj2/springproj2/controller/employeeController.java
+++ springproj2/src/main/java/com/aph/spring/proj2/springproj2/controller/employeeController.java
@@ -2,16 +2,17 @@ package com.aph.spring.proj2.springproj2.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import com.aph.spring.proj2.springproj2.model.Employee;
 import com.aph.spring.proj2.springproj2.repository.EmployeeRepo;
-import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @RequestMapping(value = "/employee")
 public class employeeController {
+
+    private boolean flag = true;
 
     @Autowired
     private EmployeeRepo employeeRepo;
@@ -35,31 +38,59 @@ public class employeeController {
     //     employeeRepo.saveAndFlush(e2);
     // }
 
+    @GetMapping("/return")
+    public ModelAndView backAndCancel(ModelAndView mav){
+        mav.setViewName("redirect:/employee/");
+        return mav;
+    }
+
     @GetMapping("/")
     public ModelAndView indexEmployee(ModelAndView mav,@ModelAttribute("employee") Employee employee) {
         Iterable<Employee> employees = employeeRepo.findAll();
-        mav.addObject("msg","");
         mav.addObject("employee", employee);
         mav.addObject("employees", employees);
+        mav.addObject("flag", flag);
         mav.setViewName("employee");
         return mav;
     }
 
     @PostMapping("/add")
     public ModelAndView addEmployee(@ModelAttribute("employee") Employee employee,ModelAndView mav) {
-        LocalDateTime dateTime = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY/MM/DD");
-        String currentDate = dateTime.format(formatter);
+        String currentDate = getCurrentDateTime();
         // Employee
-        System.out.println(employee.getPosition());
         Employee e = new Employee();
         e.setName(employee.getName());
         e.setPosition(employee.getPosition());
         e.setCreatedAt(currentDate);
         employeeRepo.saveAndFlush(e);
-        mav.addObject("msg", "Successfully added new one");
         mav.setViewName("redirect:/employee/");
         return mav;
+    }
+
+    @GetMapping("/update/{id}")
+    public ModelAndView getUpdate(@PathVariable("id") Long id,ModelAndView mav)
+    {
+        flag = false;
+        Optional<Employee> employee = employeeRepo.findById(id);
+        mav.addObject("employee", employee.get());
+        mav.setViewName("employee");
+        return mav;
+    }
+
+    @PostMapping("/update")
+    public ModelAndView update(@ModelAttribute("employee") Employee employee,ModelAndView mav){
+        String currentDate = getCurrentDateTime();
+        employee.setUpdatedAt(currentDate);
+        employeeRepo.saveAndFlush(employee);
+        mav.setViewName("redirect:/employee/");
+        return mav;
+    }
+
+    String getCurrentDateTime(){
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY/MM/DD");
+        String currentDate = dateTime.format(formatter);
+        return currentDate;
     }
     
     
